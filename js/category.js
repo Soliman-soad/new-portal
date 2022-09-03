d4b2b788e43adabbe21e742790a08d4d4bd7a713
soad <mdsolimansoad100099@gmail.com>
2022-09-03 10:46:43 +0600

    blog section added

--- js/category.js
+++ js/category.js
@@ -67,7 +67,7 @@ const viewNews = newsData => {
                   </div>
                   <div class='text-white text-lg text-bold flex items-center'>
                   <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
-                  <h4 class=" m-2">${total_view ? total_view : "N/A"}</h4>
+                  <h4 class=" m-2">${total_view ? total_view : "No views"}</h4>
                   </div>
                 </div>
                 <label for="${_id}" class="btn modal-button m-5">Read Full news</label>
